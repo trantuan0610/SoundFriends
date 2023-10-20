79a57580cf6ced90b4f893563b48e7b92edd42f2
cuong1806 <cuong.dz.1806@gmail.com>
2023-10-20 11:08:38 +0700

    build activity_register.xml

---
+++ app/src/main/java/com/example/soundfriends/auth/Login.java
@@ -0,0 +1,14 @@
+package com.example.soundfriends.auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.soundfriends.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
