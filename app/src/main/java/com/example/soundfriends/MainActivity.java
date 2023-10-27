da49b99e77e099fdd9b0759a70f4622cef5c273a
cuong1806 <cuong.dz.1806@gmail.com>
2023-10-28 00:20:24 +0700

    implement Login.java, Register.java, sharedAuthMethods.java

--- app/src/main/java/com/example/soundfriends/MainActivity.java
+++ app/src/main/java/com/example/soundfriends/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.soundfriends;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.soundfriends.auth.Login;
 import com.example.soundfriends.auth.Register;
@@ -14,41 +16,51 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthCredential;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class MainActivity extends AppCompatActivity {
     FirebaseAuth auth;
     TextView textView;
     Button btnLogout;
     FirebaseUser user;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        auth = FirebaseAuth.getInstance();
-        user = auth.getCurrentUser();
-
         textView = findViewById(R.id.textView);
         btnLogout = findViewById(R.id.logout);
 
+        //get Firebase user
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+
         if(user == null) {
-            Intent unauthIntent = new Intent(this, Login.class);
-            startActivity(unauthIntent);
-            finish();
+            goAuthActivity();
         }else {
-            textView.setText(user.getEmail());
+            String info = user.getEmail() != null ? user.getEmail() : user.getDisplayName();
+            textView.setText("Tài khoản: " + info);
         }
 
         btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 auth.signOut();
-                Intent unauthIntent = new Intent(MainActivity.this, Login.class);
-                startActivity(unauthIntent);
-                finish();
+                goAuthActivity();
             }
         });
+    }
+
+    private void goAuthActivity(){
+        Intent unauthIntent = new Intent(MainActivity.this, Login.class);
+        startActivity(unauthIntent);
+        finish();
     }
 }
--- app/src/main/java/com/example/soundfriends/auth/Register.java
+++ app/src/main/java/com/example/soundfriends/auth/Register.java
@@ -1,13 +1,17 @@
 package com.example.soundfriends.auth;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,6 +21,7 @@ import android.widget.Toast;
 
 import com.example.soundfriends.MainActivity;
 import com.example.soundfriends.R;
+import com.example.soundfriends.utils.validator;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -24,27 +29,32 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseNetworkException;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthCredential;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class Register extends AppCompatActivity {
     EditText edtEmail, edtPassword, edtPassword2;
     Button btnRegister, btnLogIn;
     ImageView btnRegisterWithGoogle;
-    FirebaseAuth mAuth;
+    FirebaseAuth firebaseAuth;
     ProgressBar pbLoadRegister;
 
-//    GoogleSignInOptions googleSignInOptions;
-//    GoogleSignInClient googleSignInClient;
+    GoogleSignInOptions googleSignInOptions;
+    GoogleSignInClient googleSignInClient;
 
     @Override
     public void onStart() {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
         if(currentUser != null){
-            HomeActivity();
+            sharedAuthMethods sharedAuthMethods = new sharedAuthMethods();
+            sharedAuthMethods.goHomeActivity(Register.this);
         }
     }
 
@@ -52,13 +62,6 @@ public class Register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-
-        //getFirebaseAuth Instance
-        mAuth = FirebaseAuth.getInstance();
-//        //getGoogleSignInOptions
-//        googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
-//        //getGoogleSignInClient
-//        googleSignInClient = GoogleSignIn.getClient(Register.this, googleSignInOptions);
 
         edtEmail = (EditText) findViewById(R.id.edtRegisterEmail);
         edtPassword = (EditText) findViewById(R.id.edtRegisterPassword);
@@ -68,6 +71,18 @@ public class Register extends AppCompatActivity {
         btnLogIn = (Button) findViewById(R.id.btnLoginInRegister);
         btnRegisterWithGoogle = (ImageView) findViewById(R.id.btnRegisterWithGoogle);
 
+        //getFirebaseAuth Instance
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        //getGoogleSignInOptions
+        googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.client_id))
+                .requestEmail()
+                .build();
+        //getGoogleSignInClient
+        googleSignInClient = GoogleSignIn.getClient(Register.this, googleSignInOptions);
+
+        //1ST WAY: NORMAL AUTHENTICATE
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -85,30 +100,30 @@ public class Register extends AppCompatActivity {
                     return;
                 }
 
-                //test commit
-
                 //set loading state
-                pbLoadRegister.setVisibility(View.VISIBLE);
-
-                mAuth.createUserWithEmailAndPassword(email, password)
+                sharedAuthMethods.setLoading(true, pbLoadRegister);
+                firebaseAuth.createUserWithEmailAndPassword(email, password)
                         .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 if (task.isSuccessful()) {
                                     // Sign in success, update UI with the signed-in user's information
-                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    FirebaseUser user = firebaseAuth.getCurrentUser();
                                     // If sign in success, display a message to the user.
                                     Toast.makeText(Register.this, "Đăng ký thành công",
                                             Toast.LENGTH_SHORT).show();
-                                    HomeActivity();
+                                    sharedAuthMethods sharedAuthMethods = new sharedAuthMethods();
+                                    sharedAuthMethods.goHomeActivity(Register.this);
                                 } else {
                                     // If sign in fails, display a message to the user.
-                                    Exception exception =  task.getException();
-                                    Toast.makeText(Register.this, exception.getMessage(),
+                                    FirebaseAuthException exception = (FirebaseAuthException) task.getException();
+                                    //validate
+                                    String errorMessage = validator.validatorMessage(exception.getErrorCode());
+                                    Toast.makeText(Register.this, errorMessage,
                                             Toast.LENGTH_LONG).show();
                                 }
                                 //hide loading state
-                                pbLoadRegister.setVisibility(View.GONE);
+                                sharedAuthMethods.setLoading(false,pbLoadRegister);
                             }
                         });
             }
@@ -123,32 +138,21 @@ public class Register extends AppCompatActivity {
             }
         });
 
+        //2ND WAY: AUTHENTICATE WITH GOOGLE
+        ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                    sharedAuthMethods sharedAuthMethods = new sharedAuthMethods();
+                    sharedAuthMethods.GoogleIntentLauncher(result, Register.this, pbLoadRegister, firebaseAuth, googleSignInClient);
+            }
+        });
+
         btnRegisterWithGoogle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                Intent GoogleSignInIntent = googleSignInClient.getSignInIntent();
-//                startActivityForResult(GoogleSignInIntent, 100);
+                Intent GoogleSignInIntent = googleSignInClient.getSignInIntent();
+                activityResultLauncher.launch(GoogleSignInIntent);
             };
         });
-    }
-
-//    @Override
-//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//
-//        if (requestCode == 100) {
-//            Task<GoogleSignInAccount> googleSignInAccountTask = GoogleSignIn.getSignedInAccountFromIntent(data);
-//            try {
-//                googleSignInAccountTask.getResult(ApiException.class);
-//            } catch (ApiException e) {
-//                Toast.makeText(Register.this, "Lỗi đăng nhập qua Google", Toast.LENGTH_SHORT).show();
-//            }
-//        }
-//    }
-
-    private void HomeActivity() {
-        Intent MainActivityIntent = new Intent(Register.this, MainActivity.class);
-        startActivity(MainActivityIntent);
-        finish();
     }
 }
