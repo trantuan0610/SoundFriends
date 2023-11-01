03768a00839d80be170bcf29be40c54bc2094ff5
cuong1806 <cuong.dz.1806@gmail.com>
2023-11-01 14:21:39 +0700

    implement Bottom Nav & SettingsFragment

--- app/src/main/java/com/example/soundfriends/adapter/BottomNavAdapter.java
+++
@@ -1,4 +0,0 @@
-package com.example.soundfriends.adapter;
-
-public class BottomNavAdapter{
-}
---
+++ app/src/main/java/com/example/soundfriends/adapter/ViewPagerAdapter.java
@@ -0,0 +1,32 @@
+package com.example.soundfriends.adapter;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.example.soundfriends.fragments.HomeFragment;
+import com.example.soundfriends.fragments.SearchFragment;
+import com.example.soundfriends.fragments.SettingsFragment;
+
+public class ViewPagerAdapter extends FragmentStateAdapter {
+    public ViewPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position){
+            case 0: return new HomeFragment();
+            case 1: return new SearchFragment();
+            case 2: return new SettingsFragment();
+            default: return new HomeFragment();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3;
+    }
+}
